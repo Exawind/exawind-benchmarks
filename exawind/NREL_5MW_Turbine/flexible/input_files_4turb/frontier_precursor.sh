990a56bf48eeea4c674d56ddfb005937a83088bc
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2025-04-23 12:52:29 -0400

    add modules on frontier

--- exawind/NREL_5MW_Turbine/flexible/input_files_4turb/frontier_precursor.sh
+++ exawind/NREL_5MW_Turbine/flexible/input_files_4turb/frontier_precursor.sh
@@ -12,6 +12,13 @@ cmd() {
   eval "$@"
 }
 
+cmd "module unload PrgEnv-cray"
+cmd "module load PrgEnv-gnu-amd/8.6.0"
+cmd "module load amd-mixed/6.1.3"
+cmd "export FI_MR_CACHE_MONITOR=memhooks"
+cmd "export FI_CXI_RX_MATCH_MODE=software"
+cmd "export MPICH_SMP_SINGLE_COPY_MODE=NONE"
+cmd "export MPICH_GPU_SUPPORT_ENABLED=0"
 cmd "export EXAWIND_MANAGER=${HOME}/exawind/exawind-manager"
 cmd "source ${EXAWIND_MANAGER}/start.sh && spack-start"
 cmd "spack env activate -d ${EXAWIND_MANAGER}/environments/exawind-dev"
