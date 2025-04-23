0872ce889d1844cc29f45190e5ebcb209d9457cc
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2025-04-23 17:35:47 -0400

    consolidate frontier scripts

--- exawind/NREL_5MW_Turbine/flexible/input_files_4turb/kestrel_hybrid.sh
+++ exawind/NREL_5MW_Turbine/flexible/input_files_4turb/kestrel_hybrid.sh
@@ -21,7 +21,7 @@ echo "Running with 124 ranks per node and 1 ranks per GPU on 32 nodes for a tota
 cmd "export EXAWIND_MANAGER=${HOME}/exawind/exawind-manager"
 cmd "source ${EXAWIND_MANAGER}/start.sh && spack-start"
 cmd "spack env activate -d ${EXAWIND_MANAGER}/environments/exawind-dev-cuda"
-cmd "spack load exawind"
+cmd "spack load exawind+amr_wind_gpu~nalu_wind_gpu"
 cmd "export MPICH_OFI_SKIP_NIC_SYMMETRY_TEST=1"
 cmd "export MPICH_GPU_SUPPORT_ENABLED=0"
 cmd "export MPICH_RANK_REORDER_METHOD=3"
